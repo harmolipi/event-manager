e946f8f8e52b19ef0b912d6dab2468a5f651c8b5
harmolipi <nikob381@gmail.com>
2021-03-14 19:11:25 -0400

    Completed Iteration 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,9 @@
 require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'EventManager Initialized!'
 
 # lines = File.readlines('event_attendees.csv')
@@ -17,6 +22,12 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
