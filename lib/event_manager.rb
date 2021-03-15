241190ba79861373ffaa7ffdd886252de8c4cf48
harmolipi <nikob381@gmail.com>
2021-03-15 02:51:05 -0400

    Finished outputting form letters to files

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,18 +1,37 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts 'EventManager Initialized!'
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-# lines = File.readlines('event_attendees.csv')
-# lines.each_with_index do |line,index|
-#   next if index == 0
-#   columns = line.split(',')
-#   name = columns[2]
-#   puts name
-# end
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exists?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager Initialized!'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -20,14 +39,18 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents.each do |row|
-  name = row[:first_name]
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
-  # if the zip code is exactly five digits, assume that it is ok
-  # if the zip code is more than five digits, truncate it to the first five digits
-  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
